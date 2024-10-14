8752f3b5c6e4731a0027746db2d085fb077a15c5
Jeevan Muthu <75674374+Jeevan2305@users.noreply.github.com>
2024-10-14 20:47:05 +0530

    Create script.js

---
+++ BMICalculator/script.js
@@ -0,0 +1,48 @@
+const h = document.querySelector("#h")
+const w = document.querySelector("#w")
+const btn = document.querySelector("#calc")
+const num = document.querySelector("#ans-num")
+const cat = document.querySelector("#ans-cat")
+
+btn.addEventListener("click", calculate)
+
+function calculate()
+{
+  
+  if(h.value > 250 || h.value < 50 || 
+     w.value > 250 || w.value < 4)
+     {
+        ans.textContent = "Invalid"
+     }
+  
+  
+  var den = Math.floor(h.value) * Math.floor(h.value) * 0.0001
+  var bmi = Math.floor(w.value)/den
+
+  num.textContent = bmi.toString()
+
+  if(bmi < 18.5)
+  {
+    cat.textContent = "Under Weight"
+  }
+  else if(bmi >= 18.5 && bmi < 25)
+  {
+    cat.textContent = "Normal"
+  }
+  else if(bmi >= 25 && bmi < 30)
+  {
+    cat.textContent = "Over Weight"
+  }
+  else if(bmi >= 30 && bmi < 40)
+  {
+    cat.textContent = "Obesity"
+  }
+  else if(bmi >= 40)
+   {
+     cat.textContent = "Extreme Obesity" 
+   }
+  else
+   {
+     cat.textContent = "Invalid" 
+   }
+}
