9ff461c1e3fc50d3d7eef21d6f2c335508ee4814
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2024-10-26 13:30:00 +0500

    Do do list app
    it uses browsers memory to store the data

--- script.js
+++ script.js
@@ -1,142 +1,66 @@
-let boxes = document.querySelectorAll(".game .box");
-let reset = document.querySelector(".game .reset");
-let container = document.querySelector(".message");
-let winner = document.querySelector(".message .winner");
-let newgame = document.querySelector(".message .bbt");
-let draw = document.querySelector(".message .draw");
-let winPattern = 
-[
-[0,1,2] ,
-[0,3,6] , 
-[0,4,8] , 
-[1,4,7] , 
-[2,4,6] ,
-[2,5,8] ,
-[3,4,5] , 
-[6,7,8] 
-]  
+let input = document.querySelector(".application input");
+let button = document.querySelector(".application button");
+let list = document.querySelector(".application #list");
 
-
-
-// players turn
-let turn = 0;                 
-for(let i=0; i<boxes.length; i++)
+function addNote()
 {
-    boxes[i].addEventListener("click" , () =>
-{
-    if(boxes[i].innerText === "")
+
+    if (input.value === '')
     {
-
-        if(turn%2 == 0)
-            {
-                boxes[i].innerText = "X"
-            }
-            
-            else
-            {
-                boxes[i].innerText = "O"
-            }
-            
+        alert("Please write something first");
     }
-    checkwinner();
-    turn++
-    console.log(turn);
-    
- 
-    if (turn === 9 )
+
+    else
     {
-        if(!checkwinner())
-        {
-        container.classList.remove("hide")
-        newgame.classList.remove("hide")
-        draw.classList.remove("hide");
-        winner.classList.add("hide")
-        disable()
-        }
-    }
-})
-}
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
 
-// reseting game
-
-reset.addEventListener("click",() =>
-{
-    enable()
-    container.classList.add("hide")
-    draw.classList.add("hide");
-    for(let i=0; i<boxes.length; i++)
-    {
-        boxes[i].innerText = "";
-    }
-    turn = 0;
-})
-
-
-newgame.addEventListener("click",() =>
-{
-    // winner.classList.add("hide")
-        container.classList.add("hide")
-        draw.classList.add("hide");
-        // newgame.classList.add("hide")
-        enable()
-    for(let i=0; i<boxes.length; i++)
-    {
-        boxes[i].innerText = "";
-    }
-    turn = 0;
-})
-
-
-
-// to disable buttons buttons aftger the games is fininshed
-const disable = ()=>
-{
-    for(let box of boxes)
-    {
-        box.disabled = true;
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+        
+        input.value = ""
+        save();
     }
 }
 
-const enable = ()=>
+
+//function to remove the note
+list.addEventListener("click" , function(e)
 {
-    for(let box of boxes)
+    if(e.target.tagName === "LI")
     {
-        box.disabled = false;
+        e.target.classList.toggle("check");
+        save();      
     }
+
+    else if (e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+   
+        
+    }
+}, false)
+
+
+
+function save()
+{
+    localStorage.setItem("data" , list.innerHTML);
 }
 
-// print detials after the games is finished
-const gamedetails = (val)=>
-    {
-        winner.innerText = val+" Wins"
-        winner.classList.remove("hide")
-        container.classList.remove("hide")
-        newgame.classList.remove("hide")
-        disable()
-    }
-    
-    // winnig logic
-    const checkwinner = ()=>
-    {
-        for (let element of winPattern)
-        {
-            let val1 =    boxes[element[0]].innerText;
-            let val2 =    boxes[element[1]].innerText;
-            let val3 =    boxes[element[2]].innerText;
-    
-            if(val1 != "" && val2 != "" && val3 != "")
-            {
-                if(val1 === val2 && val2 === val3)
-                {
-                    gamedetails(val1);
-                    return true;
-                }
-            }       
-        }
-        return false;
-    }
-    
+function show()
+{
+    list.innerHTML = localStorage.getItem("data")
 
+    const items = list.querySelectorAll("li");
+    items.forEach(item => {
+        item.querySelector("span").addEventListener("click", function () {
+            item.remove(); // Remove the list item
+            save(); // Save the current state
+        });
+    });
+}
 
-
-
+show();
